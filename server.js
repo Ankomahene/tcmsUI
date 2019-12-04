13b384ac93735d183daa2065c92a2bff5a7fea1e
ankomahene <ankomaheneshadrack@gmail.com>
2019-12-04 10:20:22 +0000

    fixed project directory path

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tcmsui'));
+app.use(express.static(__dirname + '/dist/tcmsUI'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/tcmsui/index.html'));
+res.sendFile(path.join(__dirname+'/dist/tcmsUI/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
