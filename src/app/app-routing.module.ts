a4dbc57f1eee927461446fc88008141036b1b941
ankomahene <ankomaheneshadrack@gmail.com>
2019-12-04 08:38:02 +0000

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import {HomeComponent} from "./home/home.component";
+import {CustomersComponent} from "./customers/customers.component";
+import {ProjectsComponent} from "./projects/projects.component"
+import { ReportsComponent } from './reports/reports.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',   redirectTo: '/index', pathMatch: 'full' },
+  {path: "index", component: HomeComponent},
+  {path: "customers", component: CustomersComponent},
+  {path: "projects", component: ProjectsComponent},
+  {path: "reports", component: ReportsComponent},
+  {path: "new_customer", component: NewCustomerComponent},
+  {path: "new_project", component: NewProjectComponent},
+  {path: "customer_details", component: CustomerDetailsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ReportsComponent } from './reports/reports.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    ProjectsComponent,
+    ReportsComponent,
+    NavComponent,
+    HomeComponent,
+    NewCustomerComponent,
+    NewProjectComponent,
+    CustomerDetailsComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
